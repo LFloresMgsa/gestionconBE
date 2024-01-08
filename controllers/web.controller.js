a4e01342ef220b5a9529d1b1812a29b41b7c123f
LFloresMgsa <107148012+LFloresMgsa@users.noreply.github.com>
2024-01-08 12:32:40 -0500

    first commit

---
+++ controllers/web.controller.js
@@ -0,0 +1,118 @@
+//WebController.js
+/*-------------------------------------------------
+Componente: Procedimientos No Transaccionales
+-------------------------------------------------*/
+const mysql = require("mysql");
+const sc = require("../database/StringConection");
+const path = require('path');
+const fs = require('fs');
+
+//const util = require('util');
+
+
+const db = require("../database/db.js");
+
+const ouUsuario = require("../models/sgm_usuarios.js");
+
+//get all data api with store procedure
+
+
+
+const getPath = async (req, res) => {
+    const category = req.query.category;
+  
+    if (!category) {
+      return res.status(400).json({ error: 'Falta el parámetro "category" en la solicitud.' });
+    }
+  
+    try {
+      const categoryPath = path.join(__dirname, '..', 'assets', 'documents', category);
+  
+      fs.readdir(categoryPath, (err, files) => {
+        if (err) {
+          return res.status(500).json({ error: 'Error al leer la carpeta.' });
+        }
+  
+        const fileNames = files.filter(file => file.endsWith('.pdf'));
+        res.json({ files: fileNames });
+      });
+    } catch (error) {
+      console.error('Error reading folder:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } 
+  };
+  
+  const servePDF = (req, res) => {
+    const { category, document } = req.query;
+    if (!category || !document) {
+      return res.status(400).json({ error: 'Parámetros incompletos.' });
+    }
+  
+    const filePath = path.join(__dirname, '..', 'assets', 'documents', category, document);
+  
+    if (fs.existsSync(filePath) && filePath.toLowerCase().endsWith('.pdf')) {
+      // Configura el encabezado para la respuesta PDF
+      res.setHeader('Content-Type', 'application/pdf');
+      
+      // Agrega el encabezado Content-Disposition para establecer el nombre del archivo
+      res.setHeader('Content-Disposition', `inline; filename*=UTF-8''${encodeURIComponent(document)}`);
+      console.log('Nombre del documento:', document);
+      
+      // Lee el contenido del archivo y envíalo como respuesta
+      fs.createReadStream(filePath).pipe(res);
+    } else {
+      res.status(404).json({ error: 'Archivo no encontrado.' });
+    }
+  };
+
+
+const getUsuario = async (request, response) => {
+
+
+    
+    let connection;
+    try {
+        // create mysql connection
+        connection = await mysql.createConnection(sc.dbStringConection());
+
+        var params = request.body;
+
+
+        ouUsuario.Accion = params.Accion;
+        ouUsuario.Sgm_cUsuario = params.Sgm_cUsuario;
+        ouUsuario.Sgm_cNombre = params.Sgm_cNombre;
+        ouUsuario.Sgm_cContrasena = params.Sgm_cContrasena;
+        ouUsuario.Sgm_cObservaciones = params.Sgm_cObservaciones;
+
+        connection.query("CALL sp_sgm_usuarios (?,?,?,?,?) ", [
+            ouUsuario.Accion, ouUsuario.Sgm_cUsuario, ouUsuario.Sgm_cNombre,
+            ouUsuario.Sgm_cContrasena, ouUsuario.Sgm_cObservaciones
+        ], function (error, results, fields) {
+
+            if (error) {
+
+                response.json({ error: error.message });
+
+            } else {
+
+                
+
+                response.json(results);
+            }
+        });
+    } catch (error) {
+        response.status(500);
+        response.send(error.message);
+    } finally {
+        if (connection) {
+            connection.end();
+        }
+    }
+};
+
+// export functions
+module.exports = {
+    getUsuario, 
+    getPath,
+    servePDF,
+};
---
+++ routes/evento.routes.js
@@ -0,0 +1,27 @@
+//evento.routes.js
+const express = require("express");
+
+// servicios sin transaccion
+const webController  = require("../controllers/web.controller.js");
+
+// autorizacion con jwt
+const authorization  = require("../controllers/auth.js");
+
+// midleware de autorizacion con jwt
+const checkAuth = require("../middleware/checkAuth.js");
+
+
+const router = express.Router();
+
+// servicios libres
+//router.post('/sgm_usuarios/',webController.getUsuario);
+router.post('/auth/',authorization.token);
+
+router.get('/documents', webController.getPath);
+router.get('/pdf', webController.servePDF);
+
+// servicios con seguridad JWT
+router.post('/sgm_usuarios/auth/',checkAuth,webController.getUsuario);
+//router.post('/lgm_catalogo_bs/auth/',checkAuth,webController.getCatalogo);
+
+module.exports = router;
